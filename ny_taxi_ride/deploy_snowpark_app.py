ab3bbe7f36f7cd27ae546b14de8347ab32548368
Anindya Bagchi <anindya.bagchi@gmail.com>
2025-08-31 00:31:44 +0000

    update

--- ny_taxi_ride/deploy_snowpark_app.py
+++ ny_taxi_ride/deploy_snowpark_app.py
@@ -4,10 +4,13 @@ import yaml
 
 # os.system(f"conda init")
 # os.system(f"conda activate snowpark")
-directory_path= sys.argv[1]
+
+directory_path = sys.argv[1]
 
 os.chdir(f"{directory_path}")
-# Make sure all 6 SNOWFLAKE_ environment variables are set
-# SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
+
+# This is where the flag is needed to allow shared libraries during package creation
 os.system(f"snow snowpark build --allow-shared-libraries")
-os.system(f"snow snowpark deploy --allow-shared-libraries --replace --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE")  
+
+# The deploy command does not accept the --allow-shared-libraries flag
+os.system(f"snow snowpark deploy --replace --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE")
